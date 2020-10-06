7f95db12d30c66aeaf88a7378090ddde53a12566
unknown <aelise17264@gmail.com>
2020-10-06 11:07:59 -0600

    knexfile.js & migrations

---
+++ migrations/20201006110326_init.js
@@ -0,0 +1,11 @@
+// record all changes made to the database schema
+
+//the up() function defines/holds/descripbes the changes to apploy to the db when the migration runs
+exports.up = function(knex) {
+  
+};
+
+//the down() function describes how to undo the changes
+exports.down = function(knex) {
+  
+};
